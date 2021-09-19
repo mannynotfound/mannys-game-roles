82a7a383e322ba896ff67b8ad99b90429cd9d30e
manny404 <manny@art404.com>
2021-09-19 15:38:31 -0400

    remove auth url, just update new builds

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,11 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
-const redirectUri =
-  process.env.NODE_ENV === "production"
-    ? "https%3A%2F%2Fmg-roles-nu.vercel.app%2Fapi%2Fauth%2Fcallback%2Fdiscord"
-    : "http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord";
-
 export default NextAuth({
   providers: [
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      authorizationUrl: `https://discord.com/oauth2/authorize?response_type=code&prompt=none&scope=identify%20email&redirect_uri=${redirectUri}&client_id=${process.env.DISCORD_CLIENT_ID}`,
     }),
   ],
 
